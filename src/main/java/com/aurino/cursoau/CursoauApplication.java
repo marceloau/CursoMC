f63579b243eac195458759410242b97aaf6b9aaa
Aurino <marceloaurino2207@gmail.com>
2018-02-10 13:46:58 -0300

    Projeto Criado

---
+++ src/main/java/com/aurino/cursoau/CursoauApplication.java
@@ -0,0 +1,12 @@
+package com.aurino.cursoau;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CursoauApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CursoauApplication.class, args);
+	}
+}
